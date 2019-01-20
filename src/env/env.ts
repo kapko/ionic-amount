17bbb82863890c5cf3b286611f4d437e68b11be7
kzhekshen <kzhekshen@incentient.com>
2019-01-20 17:52:59 +0600

    env fixed

--- src/env/env.ts
+++ src/env/env.ts
@@ -1,4 +1,4 @@
 export const env = {
-    // api: "https://amountapi.herokuapp.com/api/"
-    api: "http://localhost:4000/api/"
+    api: "https://amountapi.herokuapp.com/api/"
+    // api: "http://localhost:4000/api/"
 }
